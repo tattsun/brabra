d94dcc7997a9732d89fc8fff77678e4105a39545
Tojima Tatsuya <tattsun@tattsuns-mbp.local>
2015-04-02 06:22:05 +0900

    fix

--- main.go
+++ main.go
@@ -5,12 +5,8 @@ import (
 	"image"
 	"image/png"
 	"net/http"
+	"os"
 	"strconv"
-)
-
-const (
-	BASEDIR = "."
-	URL     = "http://vignette4.wikia.nocookie.net/robber-penguin-agency/images/6/6e/Small-mario.png/revision/latest?cb=20150107080404"
 )
 
 func GetImage(basedir string, uri string, level float64) (image.Image, error) {
@@ -29,14 +25,14 @@ func GetImage(basedir string, uri string, level float64) (image.Image, error) {
 	return pic, nil
 }
 
-func testHandler(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) {
 	uri := r.URL.Query().Get("t")
 	level, err := strconv.ParseFloat(r.URL.Query().Get("l"), 64)
 	if err != nil {
 		fmt.Fprintf(w, "invalid l")
 	}
 
-	img, err := GetImage(BASEDIR, uri, level)
+	img, err := GetImage(os.Args[1], uri, level)
 	if err != nil {
 		fmt.Fprintf(w, "Error %s", err)
 	}
@@ -44,6 +40,6 @@ func testHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", testHandler)
+	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
